a2bf0d950e06ec4e7f8db2627c2f19b650919a8c
asdfjoe <60941970+asdfjoe@users.noreply.github.com>
2020-03-28 11:03:11 -0400

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,28 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    phType: 'Apple'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    phType: 'Apple'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: 'Great phone to get your daily activities done',
+    phType: 'Apple'
+    
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
